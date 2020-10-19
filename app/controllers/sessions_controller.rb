91fb8185e56ade2ac39be68752ad4e20717d938c
hoangha <hoanghale1708@gmail.com>
2020-10-19 12:54:01 +0700

    chapter 10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user
-      log_in user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+    if user&.authenticate(params[:session][:password])
+      login user
     else
       flash.now[:danger] = t "controller.sessions_controller.content_invalid"
       render :new
@@ -16,5 +14,11 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  def login user
+    log_in user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+    redirect_to user
   end
 end
